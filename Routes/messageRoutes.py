753f115f7fb3cabfb54808fd0b8f93fd062a45f0
Ankitmishra-kot <75559045+Ankitmishra-kot@users.noreply.github.com>
2024-10-15 13:03:24 +0530

    Delete Routes directory

--- Routes/messageRoutes.py
+++
@@ -1,7 +0,0 @@
-# Routes/messageRoutes.py
-from fastapi import APIRouter
-from Controllers.messageController import router as message_router
-
-router = APIRouter()
-
-router.include_router(message_router, prefix="/message")
